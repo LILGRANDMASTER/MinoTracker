33afe644a2f6efa2625e34c1e55f2f2aa7747bcb
Boldov Daniil <boldov111@mail.ru>
2025-04-15 21:22:46 +0300

    slam launch use_sim_time argument

--- mino_slam/launch/mino_slam.launch.py
+++ mino_slam/launch/mino_slam.launch.py
@@ -4,8 +4,8 @@ import xacro
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import  IncludeLaunchDescription
-from launch.substitutions import PathJoinSubstitution
+from launch.actions import  IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -14,6 +14,13 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     mino_slam_pkg = get_package_share_directory('mino_slam')
     slam_parameters = os.path.join(mino_slam_pkg, 'config', 'mapper_params_online_async.yaml')
+
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true',
+    )
 
     slam = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -24,10 +31,11 @@ def generate_launch_description():
 
         launch_arguments={
             'params_file': slam_parameters,
-            'use_sim_time': 'true',
+            'use_sim_time': LaunchConfiguration('use_sim_time'),
         }.items(),
     )
 
     return LaunchDescription([
+        use_sim_time_arg,
         slam,
     ])
